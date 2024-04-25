5bc2bd3e06fa56f9c9c2ff5c764a14c351932d3c
abdelhamid20 <abdelhamidramdani17@gmail.com>
2024-04-25 16:19:31 +0100

    3 commit

--- src/app/components/AccordionM.tsx
+++ src/app/components/AccordionM.tsx
@@ -6,6 +6,7 @@ import {
 } from "@/components/ui/accordion"
 import { HiArrowPath } from "react-icons/hi2";
 import { twMerge } from "tailwind-merge";
+import { cn } from "@/lib/utils"
 
 type AccordionProps = {
   size: 'sm' | 'md' | 'lg';
@@ -18,26 +19,37 @@ type AccordionProps = {
 }
 
 
-export default function AccordionM({size, title, paragraphe, icon, display, bord, className, ...props }:AccordionProps) {
-return (
-<Accordion className={twMerge("w-[480px] h-8", size== 'sm'?"h-8":size== 'md'?"h-10":size== 'lg'?"h-12":className)} {...props } type="single" collapsible> 
-  <AccordionItem value="item-1" className={twMerge("px-4 py-1",display == 'active'?"border-b border-r border-l border-gray-300 bg-[#F4F4F4]":
-   display == 'hover'?"border border-blue-600 bg-[#F4F4F4]":
-   display == 'disabled'?"bg-gray-200 border-b border-gray-300": className,
-   bord=='top'?"rounded-t-lg":
-   bord=='bottom'?"rounded-b-lg":className)} {...props }>
-    <AccordionTrigger {...props } className={twMerge(display == 'active'?"border-b border-gray-300":display == 'hover'?"border-b border-blue-600":className)}>Choose an option</AccordionTrigger>
-    {display !== 'disabled'?
-    <AccordionContent className="w-[480px] h-[180px] py-1" {...props }>
-      <div {...props } className="w-[445px] h-[164px] bg-blue-200 border border-blue-400 rounded-md flex flex-col items-start p-4 gap-4">
-        {/* {icon === <HiArrowPath className="text-3xl min-h-6 min-w-6" {...props }/>? {...props}: "Icon"} */}
-        <HiArrowPath className="text-3xl min-h-6 min-w-6" {...props }/>
-        <h1 className="font-bold text-blue-700 text-xl" {...props }>{title}</h1>
-        <p className="text-blue-700" {...props }>{paragraphe}</p>
-      </div>
-    </AccordionContent>:""}
-  </AccordionItem>
-</Accordion>
+function AccordionM({size, title, paragraphe, icon, display, bord, className, ...props }:AccordionProps) {
+  return (
+    <Accordion type="single" collapsible> 
+      <AccordionItem value="item-1" className={twMerge("px-4 py-1 w-[480px]",
+      size== 'sm'?"h-8":size== 'md'?"h-10 content-center":size== 'lg'?"h-12 content-center":"",
+       display == 'active'?"border-b border-r border-l border-gray-300 bg-[#F4F4F4]":
+       display == 'hover'?"border border-blue-600 bg-[#F4F4F4]":
+       display == 'disabled'?"bg-gray-200 border-b border-gray-300": className,
+       bord=='top'?"rounded-t-lg":
+       bord=='bottom'?"rounded-b-lg":className)}
+        {...props }>
+        <AccordionTrigger {...props } className={twMerge(display == 'active'?"border-b border-gray-300":
+          display == 'hover'?"border-b border-blue-600":
+          display == 'disabled'?"text-gray-600":className)}>Choose an option
+        </AccordionTrigger>
+      {display !== 'disabled'?
+        <AccordionContent className="w-[480px] h-[180px] py-1" {...props }>
+          <div {...props } className="w-[445px] h-[164px] bg-blue-200 border border-blue-400 rounded-md flex flex-col items-start p-4 gap-4">
+            <HiArrowPath className="text-3xl min-h-6 min-w-6" {...props }/>
+            <h1 className="font-bold text-blue-700 text-xl" {...props }>{title}</h1>
+            <p className="text-blue-700" {...props }>{paragraphe}</p>
+          </div>
+        </AccordionContent>
+        :""}
+      </AccordionItem>
+    </Accordion>
 
-)
+  )
 }
+
+export default AccordionM;
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,15 @@ import Image from "next/image";
 import AccordionM from "./components/AccordionM";
 export default function Home() {
   return (
-    <main className="m-3 flex flex-row justify-between">
-      <AccordionM size="sm" display="active" bord="bottom" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/>
-      <AccordionM size="md" display="disabled" bord="top" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/>
-      <AccordionM size="lg" display="hover" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/>
-    </main>
+      <>
+        <AccordionM size="lg" display="disabled" bord="bottom" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/>
+        {/* <AccordionM size="md" display="active" bord="bottom" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/> */}
+        {/* <AccordionM size="lg" display="hover" bord="bottom" title="Slot component" paragraphe="Optional placeholder component. Replace it with any component using the “Component Instance” swapper, or delete if not needed."/> */}
+      </>
   );
 }
+
+
+
+
+
